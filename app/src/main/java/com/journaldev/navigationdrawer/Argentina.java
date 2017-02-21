d699874c6fc8b0ca79a0d4483a535b58d7b95a93
Memo <memovdg@gmail.com>
2017-02-21 00:36:01 -0600

    Sincronizacion de las palabras de Argentina con Firebase, ya se muestran en el Fragment

--- app/src/main/java/com/journaldev/navigationdrawer/Argentina.java
+++ app/src/main/java/com/journaldev/navigationdrawer/Argentina.java
@@ -1,8 +1,10 @@
 package com.journaldev.navigationdrawer;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +13,27 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import static java.sql.DriverManager.println;
+
 
 public class Argentina extends ListFragment {
 
+
+    DatabaseReference dref;
+    ListView listView;
     public Argentina() {
     }
 
@@ -21,14 +41,52 @@ public class Argentina extends ListFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_argentina, container, false);
-
-        String[] modismos_ar={"Bondi","Guita","Morfar","Chupar","Rati","Ramera","Chucheta","Campera",
-        "Bombacha","Calzoncillo","Boludez","Pelotudez","Pibe","Tengo lija","Birra","Churrasco"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),R.layout.rowlayout,R.id.txtitem,modismos_ar);
+        dref= FirebaseDatabase.getInstance().getReference();
+        final ArrayList<String> modismos_ar = new ArrayList<String>();
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),R.layout.rowlayout,R.id.txtitem,modismos_ar);
         setListAdapter(adapter);
         setRetainInstance(true);
+        dref.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                if(dataSnapshot.hasChildren()){
+                    Iterator<DataSnapshot> it_paises = dataSnapshot.getChildren().iterator();
+                    while (it_paises.hasNext()){
+                        DataSnapshot data_paises = (DataSnapshot) it_paises.next();
+                        if("Argentina".equals(data_paises.getKey())){
+                            Iterator<DataSnapshot> it_palabras = data_paises.getChildren().iterator();
+                            while (it_palabras.hasNext()){
+                                DataSnapshot data_palbras = (DataSnapshot) it_palabras.next();
+                                System.out.println("Datos"+data_palbras.getKey());
+                                modismos_ar.add(data_palbras.getKey());
+                                adapter.notifyDataSetChanged();
+                            }
+                        }
+                    }
+                }
 
+            }
 
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
         return rootView;
     }
 
@@ -37,7 +95,7 @@ public class Argentina extends ListFragment {
     public void onListItemClick(ListView l, View view, int position, long id){
         ViewGroup viewGroup = (ViewGroup) view;
         TextView txt = (TextView) viewGroup.findViewById(R.id.txtitem);
-        Toast.makeText(getActivity(),txt.getText().toString(), Toast.LENGTH_LONG).show();
+        Toast.makeText(getActivity(),txt.getText().toString(), Toast.LENGTH_SHORT).show();
 
 
     }
